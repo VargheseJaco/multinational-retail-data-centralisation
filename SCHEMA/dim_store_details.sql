e83296a613758eac99dd3dab664b52b1faa64e86
Varghese Jacob <varghesejacob@Vargheses-MBP.cust.communityfibre.co.uk>
2024-02-09 21:48:51 +0000

    updating dim_store_details.sql

--- SCHEMA/dim_store_details.sql
+++ SCHEMA/dim_store_details.sql
@@ -6,10 +6,12 @@ SELECT MAX(LENGTH(CAST(country_code AS TEXT)))
 FROM dim_store_details; --longest entry is 2 characters
 
 ALTER TABLE dim_store_details
-	DROP level_0,
+	DROP level_0;
+	
+ALTER TABLE dim_store_details
 	ALTER COLUMN longitude TYPE FLOAT,
 	ALTER COLUMN locality TYPE VARCHAR(255),
-	ALTER COLUMN store_code TYPE VARCHAR(11),
+	ALTER COLUMN store_code TYPE VARCHAR(12),
 	ALTER COLUMN staff_numbers TYPE SMALLINT,
 	ALTER COLUMN opening_date TYPE DATE,
 	ALTER COLUMN store_type TYPE VARCHAR(255),
